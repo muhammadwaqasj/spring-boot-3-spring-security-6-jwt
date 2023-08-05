e6b25d7b7b3b4219b2a0107f15bba51c6c8b0537
Muhammad Waqas <m.waqasjawed@gmail.com>
2023-08-05 18:02:10 +0300

    Initial Commit

---
+++ src/main/java/com/starter/security/spring/Spring3JwtApplication.java
@@ -0,0 +1,13 @@
+package com.starter.security.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3JwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starter/security/spring/Spring3JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter.security.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
